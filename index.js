244d212a4b1d32ff735a96885867ae4376f1512e
Allen2dance <79452326+Allen2dance@users.noreply.github.com>
2021-03-06 17:44:00 +0200

    Matkaappi muudatused

--- index.js
+++ index.js
@@ -1,47 +1,75 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
-
 let matkaAndmed1 = {
   nimi: "Rabamatk",
-  kirjeldus: "Lirtsuv maa",
-  pilt: "./Pildid/Tydruk kotiga 1.png"
+  kirjeldus: "Sulav asfalt voi lirtsuv maa",
+  pilt: "./Pildid/kirsipuud.jpg",
+  registreerunud: []
 }
 
 let matkaAndmed2 = {
   nimi: "Rattamatk",
-  kirjeldus: "Väntame iga päev",
-  pilt: "./Pildid/Tydruk kotiga 1.png"
+  kirjeldus: "Väntame iga päev vähemalt 40 kilomeetrit. Ja nii nädal otsa",
+  pilt: "./Pildid/rattamatk2.jpg",
+  registreerunud: []
 }
 
 let matkaAndmed3 = {
-  nimi: "Süstamatk",
-  kirjeldus: "Murrame laineid",
-  pilt: "./Pildid/Tydruk kotiga 1.png"
-
+  nimi: "Süstamakt",
+  kirjeldus: "Sõidame iga päev vähemalt 10 kilomeetrit. Ja nii nädal otsa",
+  pilt: "./Pildid/systamatk.jpg",
+  registreerunud: []
 }
 
 let koikMatkad = [
-  matkaAndmed1, 
-  matkaAndmed2, 
-  matkaAndmed3,
+	@@ -26,15 +29,52 @@ let koikMatkad = [
   {
       nimi: "Jalgsimatk",
-      kirjeldus: "Kõnnime iga päev jalad rakku",
-      pilt: "./Pildid/Tydruk kotiga 1.png"
+      kirjeldus: "Kõnnime iga päev vähemalt 10 kilomeetrit. Ja nii nädal otsa",
+      pilt: "./Pildid/vihmaneTartu.jpg",
+      registreerunud: []
   }
 ]
 
-function matkaleRegistreerunud(RegExp, res) {
-  res.send(`mittemidagi`)
+function millineParameeter(req, res) {
+  const testNumber = req.params.number;
+  res.send('Olen saanud testnumbri ' + testNumber)
+}
+
+function matkaleRegistreerumine(req, res) {
+  const valitudMatkaIndeks = req.params.matk
+  const valitudMatkaAndmed = koikMatkad[valitudMatkaIndeks]
+  if (valitudMatkaIndeks >= 0 && valitudMatkaIndeks < koikMatkad.length) {
+    return res.render('pages/registreerimine', {matk: valitudMatkaAndmed, matkaIndeks: valitudMatkaIndeks})
+  } else {
+    return res.render('pages/index', {koikMatkad})
+  }
+}
+
+function lisaMatkaja(req, res) {
+  const registreerunu = {
+    nimi: req.query.nimi,
+    email: req.query.email,
+  }
+
+  const matkaIndeks = req.query.matk
+
+  const valitudMatk = koikMatkad[matkaIndeks]
+  valitudMatk.registreerunud.push(registreerunu)
+
+  console.log('Lisatud matkaja:')
+  console.log(valitudMatk)
+
+  res.send('Matk: ' + req.query.matk + ' email: ' + req.query.email)
 }
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/', function(req, res) {res.render('pages/index', {koikMatkad}) })
-  .get('/uudised', function(req, res) {res.render('pages/uudised') })
-  .get('/kontakt', function(req, res) {res.render('pages/kontakt') })
-  .get(`/matkajad/:matk`, matkaleRegistreerunud)
+  .get('/', (req, res) => res.render('pages/index', {koikMatkad}))
+  .get('/uudised', function(req, res) { return res.render('pages/uudised') })
+  .get('/kontakt', function(req, res) { return res.render('pages/kontakt') })
+  .get('/registreerimine', function(req, res) { return res.render('pages/registreeru') })
+  .get('/registreerimine/:matk', matkaleRegistreerumine)
+  .get('/testnumber/:number', millineParameeter)
+  .get('/lisaMatkaja', lisaMatkaja)
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
