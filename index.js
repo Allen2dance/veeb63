58f8a0856442996d30a396b07ddea76fcf294612
Allen2dance <79452326+Allen2dance@users.noreply.github.com>
2021-03-08 23:08:45 +0200

    kinnitus mall

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const PORT = process.env.PORT || 5000
 
 let matkaAndmed1 = {
   nimi: "Rabamatk",
-  kirjeldus: "Dlasdfld asdlfasdl falsdkfj aldkjfadfasdfjadlfkasdlf",
+  kirjeldus: "Oitsevad kirsid rabas",
   pilt: "/Pildid/kirsipuud.jpg",
   registreerunud: []
 }
